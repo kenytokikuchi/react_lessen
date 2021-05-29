83b14edcb6335dfb8f1f85db261ffb9de887f484
kento <35658197+kenytokikuchi@users.noreply.github.com>
2021-05-29 15:47:34 +0900

    react基礎完成

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
+const App = () => {
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setfaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setfaceShowFlag(!faceShowFlag);
+  };
+
+  // 以下の制御はnumのみ変化させて制御したい→useEffectを使用
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        // faceShowFlageがfalseのときsetfaceShowを起動させる
+        faceShowFlag || setfaceShowFlag(true);
+      } else {
+        faceShowFlag && setfaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは!</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です。</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
+    </>
+  );
+};
+
+export default App;
